350107edc7e4ee68687d78a40adce6be218263e1
Selvadharshini <selvadharshini12b@gmail.com>
2024-08-18 14:09:48 +0530

    Add files via upload

---
+++ bmi app/script.js
@@ -0,0 +1,26 @@
+//"use strict";
+variable="name";
+console.log(variable);
+/*function calculatebmi(){
+    let height=document.getElementById('height').value;
+    let weight=document.getElementById('weight').value;
+    let result=(weight/(height*height)).toFixed(2);
+    if(result<18.5){
+      document.getElementById('title').innerText="Under weight 😳";
+    }
+    else if(result>18.5 && result<24.9){
+      document.getElementById('title').innerText="Normal 💪";
+    }
+    else if(result>25 && result<29.9){
+      document.getElementById('title').innerText="Over weight 😐";
+    }
+    else if(result>30 && result<35){
+      document.getElementById('title').innerText="Obesity 😣";
+    }
+    else{
+      document.getElementById('title').innerText="Severe Obesity 🤯";
+    }
+    height.value="";
+    weight.value="";
+  }
+    */
