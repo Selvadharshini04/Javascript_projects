1feb6c54688eccfe05f45be9a6f13610c2f1f128
Selvadharshini <selvadharshini12b@gmail.com>
2024-08-18 14:12:25 +0530

    Add files via upload

---
+++ live popup/script.js
@@ -0,0 +1,8 @@
+function live(){
+    let title=document.getElementById('title');
+    let btn=document.getElementById('btn');
+    console.log(btn);
+    btn.style.display="none";
+    title.innerText="Thank you for attending the live bootcamp";
+  
+  }
